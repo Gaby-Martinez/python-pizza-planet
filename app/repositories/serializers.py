ed9b015c50b8073ec847d4d3f689bd1c7a7821ca
Gaby-Martinez <gabriela.martinez@ioet.com>
2024-04-08 20:50:22 -0500

    fix: new model for beverage

--- app/repositories/serializers.py
+++ app/repositories/serializers.py
@@ -1,5 +1,5 @@
 from app.plugins import ma
-from .models import Ingredient, Size, Order, OrderDetail
+from .models import Beverage, Ingredient, OrderBeverage, OrderIngredient, Size, Order
 
 
 class IngredientSerializer(ma.SQLAlchemyAutoSchema):
@@ -7,7 +7,14 @@ class IngredientSerializer(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Ingredient
         load_instance = True
-        fields = ('_id', 'name', 'price')
+        fields = ("_id", "name", "price")
+
+
+class BeverageSerializer(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Beverage
+        load_instance = True
+        fields = ("_id", "name", "price")
 
 
 class SizeSerializer(ma.SQLAlchemyAutoSchema):
@@ -15,37 +22,45 @@ class SizeSerializer(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Size
         load_instance = True
-        fields = ('_id', 'name', 'price')
+        fields = ("_id", "name", "price")
 
 
-class OrderDetailSerializer(ma.SQLAlchemyAutoSchema):
+class OrderIngredientSerializer(ma.SQLAlchemyAutoSchema):
 
     ingredient = ma.Nested(IngredientSerializer)
 
     class Meta:
-        model = OrderDetail
+        model = OrderIngredient
         load_instance = True
-        fields = (
-            'ingredient_price',
-            'ingredient'
-        )
+        fields = ("ingredient_price", "ingredient")
+
+
+class OrderBeverageSerializer(ma.SQLAlchemyAutoSchema):
+    beverage = ma.Nested(BeverageSerializer)
+
+    class Meta:
+        model = OrderBeverage
+        load_instance = True
+        fields = ("beverage_price", "beverage")
 
 
 class OrderSerializer(ma.SQLAlchemyAutoSchema):
     size = ma.Nested(SizeSerializer)
-    detail = ma.Nested(OrderDetailSerializer, many=True)
+    ingredients = ma.Nested(OrderIngredientSerializer, many=True)
+    beverages = ma.Nested(OrderBeverageSerializer, many=True)
 
     class Meta:
         model = Order
         load_instance = True
         fields = (
-            '_id',
-            'client_name',
-            'client_dni',
-            'client_address',
-            'client_phone',
-            'date',
-            'total_price',
-            'size',
-            'detail'
+            "_id",
+            "client_name",
+            "client_dni",
+            "client_address",
+            "client_phone",
+            "date",
+            "total_price",
+            "size",
+            "ingredients",
+            "beverages",
         )
